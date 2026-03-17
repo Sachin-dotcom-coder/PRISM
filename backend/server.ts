a911ac7a755b3f9046de0f57eb19af7f34d0dd51
eSachin-dotcom-coder <sachintrinityuse@gmail.com>
2026-03-17 23:43:03 +0530

    tennis gamecard crud

--- backend/server.ts
+++ backend/server.ts
@@ -6,6 +6,7 @@ import connectDB from './src/config/db';
 import matchRoutes from "./src/routers/matchrouter";
 import volleyballMatchRoutes from "./src/routers/volleyball_router";
 import volleyballLeadRoutes from "./src/routers/volley_leade_router";
+import tennisMatchRoutes from "./src/routers/tennis_router";
 
 dotenv.config();
 
@@ -19,6 +20,7 @@ app.use(express.json());
 app.use("/api/matches", matchRoutes);
 app.use("/api/volleyball", volleyballMatchRoutes);
 app.use("/api/volleylead",volleyballLeadRoutes);
+app.use("/api/tennis", tennisMatchRoutes);
 
 const PORT = process.env.PORT || 5000;
 
