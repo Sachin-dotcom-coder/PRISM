020d41d1369641dd0998f4516c59021053663e00
Ayush Nimkar <ayushmn29@gmail.com>
2026-03-23 22:12:03 +0530

    kabaddi frontend

--- backend/src/config/db.ts
+++ backend/src/config/db.ts
@@ -2,13 +2,23 @@ import mongoose from "mongoose";
 
 const connectDB = async (): Promise<void> => {
   try {
+<<<<<<< Updated upstream
     const uri = process.env.MONGGODB_OTHERSPORTS_URI || process.env.mongodb_urlcricket || process.env.MONGO_URI || process.env.MONGODB_URI;
+=======
+
+    const uri = process.env.MONGGODB_OTHERSPORTS_URI || process.env.mongodb_urlcricket || process.env.MONGO_URI || process.env.MONGODB_URI;
+
+>>>>>>> Stashed changes
     if (!uri) {
       throw new Error("MongoDB connection string is not defined");
     }
     console.log("MONGO_URI:", uri);
     await mongoose.connect(uri as string);
     console.log("MongoDB Connected to Database");
+<<<<<<< Updated upstream
+=======
+
+>>>>>>> Stashed changes
   } catch (error) {
     console.error("Database connection failed:", error);
     process.exit(1);
